8096a3f87c5cf6f710d80186576e95ccffa83455
yosseftame <151669320+yosseftame@users.noreply.github.com>
2024-08-30 14:47:56 +0300

    Add files via upload

---
+++ toDoList/main.js
@@ -0,0 +1,42 @@
+let input = document.getElementById("input");
+let ul = document.getElementById("ul");
+function Add() {
+    if (input.value === "") {
+        alert("its empty task please add your task");
+        
+    }
+    else {
+        
+        let createEle = document.createElement("li");
+        let createSpan = document.createElement("span");
+        let createI = document.createElement("i");
+        createSpan.innerHTML = input.value;
+        createI.classList.add("fa-solid");
+        createI.classList.add("fa-x");
+        createEle.append(createSpan);
+        createEle.append(createI);
+        ul.append(createEle)
+        createEle.onclick = function () {
+            createSpan.style.textDecoration="line-through"
+        }
+        input.value=""
+        createI.onclick = function () {
+            createEle.remove();
+        }
+    }
+    
+}
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (input.value === "") {
+        alert("its empty task please add your task");
+        
+    }
+    else {
+        
+       Add()
+    }
+    }
+});
+
